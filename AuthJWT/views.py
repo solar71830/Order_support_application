e338f14d50e1ca558fe7a63a4c32c3df4f6961cd
Bartłomiej <134093917+MFSTL1@users.noreply.github.com>
2025-10-04 20:07:43 +0200

    working basic register endpoint, started endpoint securing function

--- AuthJWT/views.py
+++ AuthJWT/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import User
 import datetime
 import os
@@ -17,7 +18,9 @@ def login(request):
                 payload = {
                     "id": str(user_req.id),
                     "username": user_req.username,
-                    "exp": datetime.datetime.now() + datetime.timedelta(minutes=30)
+                    "iat": datetime.datetime.now(),
+                    "exp": datetime.datetime.now() - datetime.timedelta(minutes=30),
+                    "alg": "HS256"
 
                 }
                 private_key =os.environ.get("SECRET_KEY")
@@ -33,16 +36,25 @@ def login(request):
 
         return HttpResponse("Nieprawidłowe żądanie", status=200)
 
+@csrf_exempt
 def register(request):
     if request.method =='POST':
-        username_new = request.POST.get('username')
-        password_new = request.POST.get('password')
-        email_new = request.POST.get('email')
-        
-        if len(username_new) < 150 and len(password_new) < 150 and is_email_valid(email_new):
-            User.objects.create(username=username_new, password=password_new,email=email_new,position='user')
-
-        return HttpResponse("Utworzono nowego użytkownika",status=200)
+        try:
+            username_new = request.POST.get('username')
+            password_new = request.POST.get('password')
+            email_new = request.POST.get('email')
+            position_new =request.POST.get('position')
+            #print(f'{username_new}\n{password_new}\n{email_new}\n{position_new}')
+        except:
+            return HttpResponse("Nieprawidłowe dane rejestracji", status=400)
+        if username_new  and password_new  and is_email_valid(email_new):
+            if position_new:
+                User.objects.create(username=username_new, password=password_new,email=email_new,position=position_new)
+            else:
+                User.objects.create(username=username_new, password=password_new,email=email_new,position='user')
+            return HttpResponse("Utworzono nowego użytkownika",status=200)
+        else:
+            return HttpResponse("Wprowadzono nieprawidłowe dane", status=400)
     else:
         return HttpResponse("Nieprawidłowe żądanie", status=400)
 
@@ -53,6 +65,32 @@ def logout(request):
 def account_info(request):
     return HttpResponse("Nieprawidłowe żądanie", status=400)
 # dodać request dla użytkownika dla informacji o swoim koncie
+
+def jwt_required(func): # wrapper dla zabezpieczenia urli
+    def wrapper(request, *args,**kwargs):
+        auth_header = request.headers.get("Authorization")
+        if not auth_header or not auth_header.startswith("Authorization"):
+            return JsonResponse({"error": "Brak nagłówka autoryzacji"})
+        token = auth_header.split(" ")[1]
+        try:
+            private_key =os.environ.get("SECRET_KEY")
+            payload = jwt.decode(token,private_key,algorithm="HS256")
+            user_id = payload["id"]
+            try:
+                user_data = User.objects.get(id=user_id)
+                
+            except Exception:
+                return JsonResponse({"error":"błąd autoryzacji"})
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({"error":"Przedawniony token"})
+        except jwt.InvalidTokenError:
+            return JsonResponse({"error":"Nieprawidłowy token"})
+        if user_data:
+            return func(request,*args,**kwargs)
+    return wrapper
+
+
+
 
 def is_email_valid(email: str) -> bool:
     try:
