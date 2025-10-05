eb0419581bc00b877fa0c118db485ad3a6097cc4
Bartłomiej <134093917+MFSTL1@users.noreply.github.com>
2025-10-05 19:48:52 +0200

    working login, register, account_info, no signature, new token definition

--- AuthJWT/urls.py
+++ AuthJWT/urls.py
@@ -1,12 +1,13 @@
 
 
 from django.urls import path
-from .views import login,register,logout
+from .views import login,register,logout,account_info
 
 
 urlpatterns = [
     path('login/',login,name='login'),
     path('register/',register,name='register'),
     path('logout/',logout,name='logout'),
+    path('account-info/',account_info,name='logout'),
 
 ]
--- AuthJWT/views.py
+++ AuthJWT/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
 import datetime
-import os
+import time
+#import os
 import jwt
+#import base64
 
 
 @csrf_exempt
@@ -15,27 +17,34 @@ def login(request):
         username_req = request.POST.get('username')
         password_req = request.POST.get('password')
         try:
+            timestamp_new = int(time.time())
             user_req = User.objects.get(username = username_req)
+            private_key ="django-insecure-14g0@on!f@q-cr%&rvnd%v1vy*-jpt5dy-wx8(qlm5urg)nrm_"
             if (str(user_req.password) == str(password_req)):
                 temp_id = str(user_req.id)
                 temp_username = str(user_req.username)
-                payload = {
-                    "id": temp_id,
-                    "username": temp_username,
-                    "iat": datetime.datetime.now(),
-                    "exp": datetime.datetime.now() + datetime.timedelta(minutes=30),
-                    
+                headers_jwt = {
+                    "alg":"HS256",
+                    "typ":"JWT"
+
 
                 }
-                private_key =os.environ.get("SECRET_KEY")
-                jwt_key = jwt.encode(payload,key=str(private_key),algorithm="HS256")
+                jwt_payload = {
+                    "sub": temp_id,
+                    "name": temp_username,
+                    "iat":timestamp_new,
+                    "exp": timestamp_new + 3600
+
+                }
+                jwt_token = jwt.encode(payload=jwt_payload,key=private_key,algorithm="HS256",headers=headers_jwt )
+                
             else:
                 return HttpResponse("Nieprawidłowe hasło",status=400)
         except User.DoesNotExist:
             return HttpResponse("Nie znaleziono użytkownika",status=400)
         
         
-        return JsonResponse({"token":jwt_key},status=200)
+        return JsonResponse({"token":jwt_token},status=200)
     else:
 
         return HttpResponse("Nieprawidłowe żądanie", status=200)
@@ -48,7 +57,6 @@ def register(request):
             password_new = request.POST.get('password')
             email_new = request.POST.get('email')
             position_new =request.POST.get('position')
-            #print(f'{username_new}\n{password_new}\n{email_new}\n{position_new}')
         except:
             return HttpResponse("Nieprawidłowe dane rejestracji", status=400)
         if username_new  and password_new  and is_email_valid(email_new):
@@ -66,34 +74,39 @@ def logout(request):
     return HttpResponse("Nieprawidłowe żądanie", status=400)
 # dodać blacklist dla zużytych tokenów który cyklicznie się czyści
 
-def account_info(request):
-    return HttpResponse("Nieprawidłowe żądanie", status=400)
-# dodać request dla użytkownika dla informacji o swoim koncie
-
 def jwt_required(func): # wrapper dla zabezpieczenia urli
     def wrapper(request, *args,**kwargs):
         auth_header = request.headers.get("Authorization")
-        if not auth_header or not auth_header.startswith("Authorization"):
+        if not auth_header:
             return JsonResponse({"error": "Brak nagłówka autoryzacji"})
-        token = auth_header.split(" ")[1]
+        if(auth_header.startswith("Bearer")):
+            token = auth_header.split(" ")[1] #dla Bearer ...token...
+        else:
+            token = auth_header
         try:
-            private_key =os.environ.get("SECRET_KEY")
-            payload = jwt.decode(token,private_key,algorithm="HS256")
-            user_id = payload["id"]
-            try:
-                user_data = User.objects.get(id=user_id)
-                
-            except Exception:
-                return JsonResponse({"error":"błąd autoryzacji"})
+            private_key = "django-insecure-14g0@on!f@q-cr%&rvnd%v1vy*-jpt5dy-wx8(qlm5urg)nrm_"
+            payload = jwt.decode(token,private_key,algorithms="HS256")
+            request.jwt_payload = payload
         except jwt.ExpiredSignatureError:
             return JsonResponse({"error":"Przedawniony token"})
         except jwt.InvalidTokenError:
             return JsonResponse({"error":"Nieprawidłowy token"})
-        if user_data:
-            return func(request,*args,**kwargs)
+        except Exception:
+            return JsonResponse({"error":"Błąd"})
+    
+        return func(request,*args,**kwargs)
     return wrapper
 
-
+@csrf_exempt
+@jwt_required
+def account_info(request):
+    if request.method =='GET':
+         jwt_payload = getattr(request, 'jwt_payload', None)
+         username_req = jwt_payload.get("name")
+         data = User.objects.get(username =username_req)
+         return JsonResponse({"info":str(data)},status = 200)
+    else:
+         return HttpResponse("Nieprawidłowe żądanie", status=400)
 
 
 def is_email_valid(email: str) -> bool:
