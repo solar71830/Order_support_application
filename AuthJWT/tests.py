756670d8de47a141036521712373c82c065560f9
Bartłomiej <134093917+MFSTL1@users.noreply.github.com>
2025-12-02 22:11:46 +0100

    first test and changes to the requirements

--- AuthJWT/tests.py
+++ AuthJWT/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from AuthJWT.models import User
+import pytest
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_login(client):
+    user, created = User.objects.get_or_create(
+        username="testadmin",
+        defaults={
+            "password": "admin123",
+            "email": "testadmin@example.com",
+            "position": "admin",
+            "role": "admin"
+        }
+    )
+    response = client.post(
+        "/login/",
+        {"username": "testadmin", "password": "admin123"}
+    )
+    assert response.status_code == 200
+    print(response.json()["token"])
