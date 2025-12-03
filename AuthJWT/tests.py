5891e67cbb4bc93de9cc68bff333bd8825c0b88d
Bartłomiej <134093917+MFSTL1@users.noreply.github.com>
2025-12-03 20:02:01 +0100

    finished the test and removed .pyc files

--- AuthJWT/tests.py
+++ AuthJWT/tests.py
@@ -19,38 +19,60 @@ def test_login(client):
         "/login/",
         {"username": "testadmin", "password": "admin123"}
     )
-    print(response.json()["token"])
+    print("\nLogin",response.json()["token"])
     assert response.status_code == 200
 
 
     token = response.json()["token"]
 
     # test tworzenia nowego użytkownika
-    create_user = client.post("/register/", {"username": "test_user", "password": "Zaq12wsx", "email": "testuser@example.com", "position": "user", "role": "user"}, headers={
+    create_user = client.post("/register/", data={"username": "test_user", "password": "Zaq12wsx", "email": "testuser@example.com", "position": "user", "role": "user"}, headers={
         "Authorization": f"Bearer {token}"
     },)
-    print(create_user)
+    print("\nCreate-User",create_user)
     assert create_user.status_code == 200
 
     # test uzyskania informacji o użytkowniku
     user_info = client.get("/account-info/?username=test_user", headers={
         "Authorization": f"Bearer {token}"
     },)
-    print(user_info.json())
+    print("\nUser-info", user_info.json())
     assert user_info.status_code == 200
 
     #test zmienienia danych użytkownika, w tym wypadku z typu user na admin
-    edit_user = client.post("/account-edit/",{"username": "test_user","password":"a113","position":"admin","role":"admin"}, headers={
+    edit_user = client.post("/account-edit/", data={"username": "test_user","password":"a113","position":"admin","role":"admin"}, headers={
         "Authorization": f"Bearer {token}"
     },)
-    print(edit_user)
+    print("\nEdit", edit_user)
     assert edit_user.status_code == 200
 
     logout_message = client.post("/logout/", headers={
         "Authorization": f"Bearer {token}"
     },)
-    print(logout_message)
+    print("\nLogout", logout_message)
     assert logout_message.status_code == 200
+
+    # testowanie użytkownika user
+
+    response2 = client.post(
+        "/login/",
+        {"username": "test_user", "password": "a113"}
+    )
+    print("\nLogin", response2.json()["token"])
+    assert response2.status_code == 200
+
+    token2 = response2.json()["token"]
+
+    # uzyskiwanie dostępu do listy użytkowników
+    user_list = client.get("/users-list/", headers={"Authorization": f"Bearer {token2}"})
+    print("\nUser-list", user_list.json())
+    assert user_list.status_code == 200
+
+    #usuwanie użytkownika
+    user_delete_message = client.post("/account-delete/",data={"username":"testadmin"}, headers={"Authorization": f"Bearer {token2}"})
+    print("\nDelete", user_delete_message)
+    assert user_delete_message.status_code == 200
+
 
 
     
